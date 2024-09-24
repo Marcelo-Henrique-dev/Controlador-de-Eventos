487ef548e43a029ebbe620128924772a7f2e18d2
Marcelo Henrique <dvmarcelohenrique@gmail.com>
2024-09-24 11:34:12 -0300

    cirado listar eventos

--- interfaces/IEventoServices.java
+++ interfaces/IEventoServices.java
@@ -1,10 +1,8 @@
 package interfaces;
 
-import entidades.Evento;
-
 public interface IEventoServices {
     public void cadastrarEventoNoRepositorio();
-    public Evento listarEventos();
+    public void listarEventos();
     public void editarEventos();
     public void apagarEvento();
 }
