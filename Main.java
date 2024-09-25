490ffc5d88b79418e4bcb0e26d40ff2958d4a8d7
Marcelo Henrique <dvmarcelohenrique@gmail.com>
2024-09-24 23:30:35 -0300

    completo

--- Main.java
+++ Main.java
@@ -3,16 +3,23 @@ import java.util.Scanner;
 import exceptions.InvalidEventoTypeException;
 import servicos.EventosServices;
 
-public class Main{
-    
+public class Main {
+
     public static Scanner scanner = new Scanner(System.in);
     public static EventosServices eventosServices = new EventosServices();
+
     public static void main(String[] args) throws InvalidEventoTypeException {
         int opc;
-        do{
+        do {
+            System.out.println("=================================");
+            System.out.println("| Projeto feito por: |");
+            System.out.println("| Marcelo Henrique | Aquiles Arruda | José Renato | Adriano Ramos |");
+            System.out.println("=================================");
             System.out.println("|======- Menu -======|");
-            System.out.println("| 1 - Cadastra evento | 2 - Visualizar eventos | 3 - Editar Evento | 4 - Cancelar Evento |");
-            System.out.println("| 5 - Cadastrar Participante | 6 - Listar Participantes | 7 - Editar Participantes | 8 - Apagar Participantes |");
+            System.out.println(
+                    "| 1 - Cadastra evento | 2 - Visualizar eventos | 3 - Editar Evento | 4 - Cancelar Evento |");
+            System.out.println(
+                    "| 5 - Cadastrar Participante | 6 - Listar Participantes | 7 - Editar Participantes | 8 - Apagar Participantes |");
             System.out.println("| 0 - Sair |");
             opc = scanner.nextInt();
             scanner.nextLine();
@@ -49,7 +56,7 @@ public class Main{
                     System.out.println("Opção Inválida");
                     break;
             }
-        }while(opc != 0);
+        } while (opc != 0);
     }
 
 }
